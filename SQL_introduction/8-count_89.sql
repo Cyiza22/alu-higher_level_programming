fc350a04315fe6fe60c46f4dfca2dc186605c9aa
Cyiza22 <h.cyiza@alustudent.com>
2023-08-02 12:09:31 -0700

    8-count_89.sql

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
--- A script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
--- Query to insert a new row
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+-- A script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server
+-- Query to display records of a table
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
