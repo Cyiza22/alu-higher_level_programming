5fdbf2266ba8938787d38b7f8c2bf41e1ef5d624
Cyiza22 <h.cyiza@alustudent.com>
2023-09-29 12:43:58 -0700

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2); // Get command-line arguments, excluding 'node' and the script filename
-
-if (args.length === 0) {
-  console.log("No argument");
-} else if (args.length === 1) {
-  console.log("Argument found");
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
-
